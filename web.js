0b3465ce320e8082feea9b52410d8f6e752b6636
ee7klt <kltah1@gmail.com>
2013-07-09 01:02:03 +0800

    Modified web.js to read from index.html
    using readFileSync

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var infile = "index.html";
+var text = fs.readFileSync('index.html','utf8')
+console.log("\nRead: " + text + "From: " + infile);
+
+
+
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
